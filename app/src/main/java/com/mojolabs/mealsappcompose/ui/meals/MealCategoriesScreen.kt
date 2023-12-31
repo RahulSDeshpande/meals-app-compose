9b7728814cd4419da7eaec11459dc9508ce76cfe
Rahul Deshpande <rahul.deshpande92@gmail.com>
2023-12-31 11:39:49 +0400

    :art: Add meal categories description in the list

--- app/src/main/java/com/mojolabs/mealsappcompose/ui/meals/MealCategoriesScreen.kt
+++ app/src/main/java/com/mojolabs/mealsappcompose/ui/meals/MealCategoriesScreen.kt
@@ -1,6 +1,7 @@
 package com.mojolabs.mealsappcompose.ui.meals
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,15 +10,22 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -56,6 +64,8 @@ fun MealCategoriesScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun MealCategories(mealCategory: MealCategory) {
+    val isExpanded = remember { mutableStateOf(false) }
+
     Card(
         shape = RoundedCornerShape(8.dp),
         modifier = Modifier
@@ -68,20 +78,38 @@ fun MealCategories(mealCategory: MealCategory) {
                 // painter = painterResource(id = photoResId),
                 painter = rememberAsyncImagePainter(model = mealCategory.imageUrl),
                 contentDescription = "Meal category image",
-                modifier = Modifier.size(100.dp, 100.dp).padding(4.dp),
+                modifier = Modifier
+                    .size(100.dp, 100.dp)
+                    .padding(4.dp),
                 contentScale = ContentScale.Inside
             )
             Column(
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
-                    .fillMaxWidth()
-                    .padding(16.dp)
+                    .fillMaxWidth(0.8f)
+                    .padding(4.dp)
             ) {
                 Text(
                     text = mealCategory.name,
                     style = MaterialTheme.typography.titleLarge
                 )
+                Text(
+                    text = mealCategory.description,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.alpha(0.7f),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = if (isExpanded.value) 10 else 2
+                )
             }
+
+            Icon(
+                imageVector = Icons.Filled.KeyboardArrowUp,
+                contentDescription = "Expand row icon",
+                modifier = Modifier
+                    .padding(start = 16.dp, top = 32.dp, end = 16.dp, bottom = 16.dp)
+                    .align(Alignment.Top)
+                    .clickable { isExpanded.value = isExpanded.value.not() }
+            )
         }
     }
 }
