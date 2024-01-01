ddc9fc025d3487714d75fa5f951cd1d3b43b115a
Rahul Deshpande <rahul.deshpande92@gmail.com>
2024-01-01 12:14:29 +0400

    :art: Animate meal category image

--- app/src/main/java/com/mojolabs/mealsappcompose/ui/meals/details/MealCategoryDetailsScreen.kt
+++ app/src/main/java/com/mojolabs/mealsappcompose/ui/meals/details/MealCategoryDetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.mojolabs.mealsappcompose.ui.meals.details
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -8,6 +9,8 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,17 +19,20 @@ import com.mojolabs.mealsappcompose.model.MealCategory
 import com.mojolabs.mealsappcompose.ui.theme.MealsAppComposeTheme
 
 @Composable
-fun MealCategoryDetailsScreen(
-    mealCategory: MealCategory?
-    // mealCategoryId: String?,
-    // navController: NavHostController?
-) {
+fun MealCategoryDetailsScreen(mealCategory: MealCategory?) {
+    val isExpanded = remember { mutableStateOf(false) }
+    val imageSize =
+        animateDpAsState(
+            targetValue = if (isExpanded.value) 200.dp else 100.dp,
+            label = ""
+        )
+
     Column {
         Card(modifier = Modifier.padding(16.dp)) {
             Image(
                 painter = rememberAsyncImagePainter(model = mealCategory?.imageUrl),
                 contentDescription = "Meal image",
-                modifier = Modifier.size(200.dp)
+                modifier = Modifier.size(imageSize.value)
             )
         }
         Text(
@@ -34,7 +40,7 @@ fun MealCategoryDetailsScreen(
             modifier = Modifier.padding(16.dp)
         )
         Button(
-            onClick = {},
+            onClick = { isExpanded.value = isExpanded.value.not() },
             modifier = Modifier.padding(16.dp)
         ) {
             Text(text = "Change state of meal profile picture")
