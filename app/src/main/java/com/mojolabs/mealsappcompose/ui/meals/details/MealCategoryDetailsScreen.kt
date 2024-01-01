1960fdfc49296d82d8a3f5c4b7aca4d2e713f260
Rahul Deshpande <rahul.deshpande92@gmail.com>
2024-01-01 12:26:14 +0400

    :art: Animate image size, color & border width using transition-state

--- app/src/main/java/com/mojolabs/mealsappcompose/ui/meals/details/MealCategoryDetailsScreen.kt
+++ app/src/main/java/com/mojolabs/mealsappcompose/ui/meals/details/MealCategoryDetailsScreen.kt
@@ -1,10 +1,14 @@
 package com.mojolabs.mealsappcompose.ui.meals.details
 
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -12,7 +16,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.mojolabs.mealsappcompose.model.MealCategory
@@ -20,19 +26,45 @@ import com.mojolabs.mealsappcompose.ui.theme.MealsAppComposeTheme
 
 @Composable
 fun MealCategoryDetailsScreen(mealCategory: MealCategory?) {
-    val isExpanded = remember { mutableStateOf(false) }
-    val imageSize =
-        animateDpAsState(
-            targetValue = if (isExpanded.value) 200.dp else 100.dp,
+    val enumMealImageState = remember { mutableStateOf(EnumMealImageState.NORMAL) }
+
+    val transitionState =
+        updateTransition(
+            targetState = enumMealImageState,
+            label = ""
+        )
+
+    val imageSizeDpState =
+        transitionState.animateDp(
+            targetValueByState = { it.value.size },
+            label = ""
+        )
+
+    val colorState =
+        transitionState.animateColor(
+            targetValueByState = { it.value.color },
+            label = ""
+        )
+
+    val borderState =
+        transitionState.animateDp(
+            targetValueByState = { it.value.borderWidth },
             label = ""
         )
 
     Column {
-        Card(modifier = Modifier.padding(16.dp)) {
+        Card(
+            modifier = Modifier.padding(16.dp),
+            shape = CircleShape,
+            border = BorderStroke(
+                width = borderState.value,
+                color = colorState.value
+            )
+        ) {
             Image(
                 painter = rememberAsyncImagePainter(model = mealCategory?.imageUrl),
                 contentDescription = "Meal image",
-                modifier = Modifier.size(imageSize.value)
+                modifier = Modifier.size(imageSizeDpState.value)
             )
         }
         Text(
@@ -40,7 +72,9 @@ fun MealCategoryDetailsScreen(mealCategory: MealCategory?) {
             modifier = Modifier.padding(16.dp)
         )
         Button(
-            onClick = { isExpanded.value = isExpanded.value.not() },
+            onClick = {
+                enumMealImageState.value = enumMealImageState.value.reverse()
+            },
             modifier = Modifier.padding(16.dp)
         ) {
             Text(text = "Change state of meal profile picture")
@@ -54,4 +88,23 @@ fun MealsCategoriesScreenPreview() {
     MealsAppComposeTheme {
         MealCategoryDetailsScreen(mealCategory = null)
     }
+}
+
+enum class EnumMealImageState(
+    val color: Color,
+    val size: Dp,
+    val borderWidth: Dp
+) {
+    NORMAL(
+        color = Color.Gray,
+        size = 120.dp,
+        borderWidth = 8.dp
+    ),
+    EXPANDED(
+        color = Color.Green,
+        size = 200.dp,
+        borderWidth = 24.dp
+    );
+
+    fun reverse() = if (this == NORMAL) EXPANDED else NORMAL
 }
